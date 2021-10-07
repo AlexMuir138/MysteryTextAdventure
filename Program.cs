3b47a4bcfaf7143322d0d88f564efbce288c1747
Alex <muirkat138@gmail.com>
2021-10-07 11:31:38 -0600

    the end

--- Program.cs
+++ Program.cs
@@ -24,6 +24,37 @@ namespace MysteryTextAdventure
       Console.WriteLine("Type OPEN or KNOCK:");
       string doorChoice = Console.ReadLine();
       string loudDoor = doorChoice.ToUpper();
+      if(loudDoor == "KNOCK"){
+        Console.WriteLine("A voice behind the door speaks. It says, \"Answer this riddle: ");
+        Console.WriteLine("Poor people have. Rich people need it. If you eat it you die. What is it?");
+        Console.WriteLine("Type your answer: ");
+        string riddleAnswer = Console.ReadLine();
+        string loudAnswer = riddleAnswer.ToUpper();
+        if(loudAnswer == "NOTHING"){
+          Console.WriteLine("The door opens and NOTHING is there. You turn off the light and run back to your room and lock the door. THE END.");
+          } else {
+            Console.WriteLine("You answered incorrectly. The door does not open.  THE END.");;
+        }
+      } else if(loudDoor == "OPEN"){
+        Console.WriteLine("The door is locked! See if one of your three keys will open it.");
+        Console.WriteLine("Enter a number between 1 and 3");
+        string keyChoice = Console.ReadLine();
+        string loudKey = keyChoice.ToUpper();
+        switch(loudKey){
+          case "1":
+            Console.WriteLine("You chose the first key. Lucky you. The door opens and NOTHING is there. Strange...   THE END.");
+            break;
+          case "2":
+            Console.WriteLine("You chose the second key. The door does not open.  THE END.");
+            break;
+          case "3":
+            Console.WriteLine("You chose the third key. The door does not open.  THE END.");
+            break;
+          default:
+            Console.WriteLine("Nope. You invalid. THE END.");
+                break;
+        }
+      }
         }
     }
 }
